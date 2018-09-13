dcf54592ce53189f841ca501b23d66bf4aa6001a
Francisco Alonzo <franalonzo96@gmail.com>
2018-09-13 08:19:35 -0600

    Añadiendo NavBar

--- app/src/main/java/android/estructurasii/lab1ed2/MainActivity.java
+++ app/src/main/java/android/estructurasii/lab1ed2/MainActivity.java
@@ -1,13 +1,22 @@
 package android.estructurasii.lab1ed2;
 
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private DrawerLayout mLayout;
+    private ListView mDrawerList;
+    private String []titles;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
+        mDrawerList = (ListView) findViewById(R.id.left_drawer);
+        //Adaptador de NavBar
+        
     }
 }
