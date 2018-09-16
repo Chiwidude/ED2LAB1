63f46806338307e1a68bb15d4e23fac2ca9c7336
Francisco Alonzo <franalonzo96@gmail.com>
2018-09-15 23:20:52 -0600

    Añadiendo navigation_bar

--- app/src/main/java/android/estructurasii/lab1ed2/MainActivity.java
+++ app/src/main/java/android/estructurasii/lab1ed2/MainActivity.java
@@ -1,22 +1,38 @@
 package android.estructurasii.lab1ed2;
 
+import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
     private DrawerLayout mLayout;
-    private ListView mDrawerList;
-    private String []titles;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        android.support.v7.widget.Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
         setContentView(R.layout.activity_main);
-        mLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
-        mDrawerList = (ListView) findViewById(R.id.left_drawer);
-        //Adaptador de NavBar
-        
+        mLayout = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,mLayout,toolbar,
+                R.string.navigation_drawer_open,R.string.navigation_drawer_close);
+        mLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(mLayout.isDrawerOpen(GravityCompat.START)){
+            mLayout.closeDrawer(GravityCompat.START);
+        } else{
+            super.onBackPressed();
+        }
+
+
     }
 }
